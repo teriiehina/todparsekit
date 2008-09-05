c47e29ec4d7fdd86e393162b6d9c6486c31d0663
todd.ditchendorf <todd.ditchendorf@7c5183cd-b054-0410-b1f0-833f5c61cfe1>
2008-09-05 16:58:01 +0000

    adding xml syntax coloring. not working yet

---
+++ test/TDXmlSyntaxColoring.h
@@ -0,0 +1,28 @@
+//
+//  TDXmlSyntaxColoring.h
+//  TDParseKit
+//
+//  Created by Todd Ditchendorf on 8/28/08.
+//  Copyright 2008 __MyCompanyName__. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+@class TDTokenizer;
+@class TDToken;
+
+@interface TDXmlSyntaxColoring : NSObject {
+	TDTokenizer *tokenizer;
+	NSMutableArray *stack;
+	TDToken *ltToken;
+	TDToken *gtToken;
+	NSMutableAttributedString *result;
+	NSDictionary *tagAttributes;
+	NSDictionary *textAttributes;
+}
+- (NSAttributedString *)parse:(NSString *)s;
+
+@property (nonatomic, retain) NSMutableAttributedString *result;
+@property (nonatomic, retain) NSDictionary *tagAttributes;
+@property (nonatomic, retain) NSDictionary *textAttributes;
+@end
