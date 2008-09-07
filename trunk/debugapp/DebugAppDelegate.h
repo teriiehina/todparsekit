4dc0f3fb61f725f61a70ffa566f00dc01772cc20
todd.ditchendorf <todd.ditchendorf@7c5183cd-b054-0410-b1f0-833f5c61cfe1>
2008-09-07 17:59:36 +0000

    adding xml syntax coloring. partially working

--- trunk/debugapp/DebugAppDelegate.h
+++ trunk/debugapp/DebugAppDelegate.h
@@ -9,7 +9,9 @@
 #import <Cocoa/Cocoa.h>
 
 @interface DebugAppDelegate : NSObject {
-
+	NSAttributedString *displayString;
 }
 - (IBAction)run:(id)sender;
+
+@property (retain) NSAttributedString *displayString;
 @end
