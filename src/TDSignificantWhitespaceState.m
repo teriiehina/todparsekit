b0f08b20216b0eb5fb80c8dde4245b8b543cb0fe
todd.ditchendorf <todd.ditchendorf@7c5183cd-b054-0410-b1f0-833f5c61cfe1>
2008-09-08 07:16:10 +0000

    adding xml syntax coloring. partially working

--- src/TDSignificantWhitespaceState.m
+++ src/TDSignificantWhitespaceState.m
@@ -24,7 +24,19 @@
 
 
 - (NSString *)debugDescription {
-	return [NSString stringWithFormat:@"<Whitespace %C%@%C>", 0x00ab, self.value, 0x00bb];
+	NSString *typeString = nil;
+	if (self.isNumber) {
+		typeString = @"Number";
+	} else if (self.isQuotedString) {
+		typeString = @"Quoted String";
+	} else if (self.isSymbol) {
+		typeString = @"Symbol";
+	} else if (self.isWord) {
+		typeString = @"Word";
+	} else if (self.isWhitespace) {
+		typeString = @"Whitespace";
+	}
+	return [NSString stringWithFormat:@"<%@ %C%@%C>", typeString, 0x00ab, self.value, 0x00bb];
 }
 
 @end
