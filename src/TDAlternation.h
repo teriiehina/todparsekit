56e1c5378d8d09f34473dd25e9f8ad2f90a5c1d1
todd.ditchendorf <todd.ditchendorf@7c5183cd-b054-0410-b1f0-833f5c61cfe1>
2008-09-30 23:36:51 +0000

    more headerdocs - parser stuff

--- src/TDAlternation.h
+++ src/TDAlternation.h
@@ -9,8 +9,20 @@
 #import <Foundation/Foundation.h>
 #import <TDParseKit/TDCollectionParser.h>
 
+/*!
+    @class       TDAlternation 
+    @superclass  TDCollectionParser
+    @abstract    A <tt>TDAlternation</tt> object is a collection of parsers, any one of which can successfully match against an assembly.
+    @discussion  A <tt>TDAlternation</tt> object is a collection of parsers, any one of which can successfully match against an assembly.
+*/
 @interface TDAlternation : TDCollectionParser {
 
 }
+
+/*!
+    @method     alternation
+    @abstract   Convenience factory method for initializing an autoreleased <tt>TDAlternation</tt> parser.
+    @result     an initialized autoreleased <tt>TDAlternation</tt> parser.
+*/
 + (id)alternation;
 @end
