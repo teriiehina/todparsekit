4c2651e0cd7a5fa36de827e65a007c3c90fc0f82
todd.ditchendorf <todd.ditchendorf@7c5183cd-b054-0410-b1f0-833f5c61cfe1>
2008-09-30 23:42:36 +0000

    more headerdocs - parser stuff

--- src/TDRepetition.h
+++ src/TDRepetition.h
@@ -9,15 +9,39 @@
 #import <Foundation/Foundation.h>
 #import <TDParseKit/TDParser.h>
 
+/*!
+    @class       TDRepetition 
+    @superclass  TDParser
+    @abstract    A <tt>TDRepetition</tt> matches its underlying parser repeatedly against a assembly.
+    @discussion  A <tt>TDRepetition</tt> matches its underlying parser repeatedly against a assembly.
+*/
 @interface TDRepetition : TDParser {
 	TDParser *subparser;
 	id preAssembler;
 	SEL preAssemblerSelector;
 }
+
+/*!
+	@method     repetition
+	@abstract   Convenience factory method for initializing an autoreleased <tt>TDRepetition</tt> parser.
+	@result     an initialized autoreleased <tt>TDRepetition</tt> parser.
+*/
 + (id)repetition;
+
+/*!
+    @method     repetitionWithSubparser:
+    @abstract   Convenience factory method for initializing an autoreleased <tt>TDRepetition</tt> parser to repeatedly match against subparser <tt>p</tt>.
+    @param      p the subparser against wich to repeatedly match
+    @result     an initialized autoreleased <tt>TDRepetition</tt> parser.
+*/
 + (id)repetitionWithSubparser:(TDParser *)p;
 
-// designated initializer
+/*!
+    @method     initWithSubparser:
+    @abstract   Designated Initializer. Initialize a <tt>TDRepetition</tt> parser to repeatedly match against subparser <tt>p</tt>.
+    @discussion Designated Initializer. Initialize a <tt>TDRepetition</tt> parser to repeatedly match against subparser <tt>p</tt>.
+	@param      p the subparser against wich to repeatedly match
+	@result     an initialized <tt>TDRepetition</tt> parser.
+*/
 - (id)initWithSubparser:(TDParser *)p;
-
 @end
