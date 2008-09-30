262b30736749a05803c98b1876989771876b861f
todd.ditchendorf <todd.ditchendorf@7c5183cd-b054-0410-b1f0-833f5c61cfe1>
2008-09-30 20:34:54 +0000

    adding header docs for much of tokenization stuff

--- src/TDWordOrReservedState.h
+++ src/TDWordOrReservedState.h
@@ -9,8 +9,20 @@
 #import <Foundation/Foundation.h>
 #import <TDParseKit/TDWordState.h>
 
+/*!
+    @class       TDWordOrReservedState 
+    @superclass  TDWordState
+    @abstract    Override <tt>TDWordState</tt> to return known reserved words as tokens of type <tt>TDTT_RESERVED</tt>.
+    @discussion  Override <tt>TDWordState</tt> to return known reserved words as tokens of type <tt>TDTT_RESERVED</tt>.
+*/
 @interface TDWordOrReservedState : TDWordState {
 	NSMutableSet *reservedWords;
 }
+
+/*!
+    @method     addReservedWord:
+    @abstract   Adds the specified string as a known reserved word.
+    @param      s reserved word to add
+*/
 - (void)addReservedWord:(NSString *)s;
 @end
