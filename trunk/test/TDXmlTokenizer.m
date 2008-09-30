0dc6da008c369b8c9ad6502e79e7571c15d0fb77
todd.ditchendorf <todd.ditchendorf@7c5183cd-b054-0410-b1f0-833f5c61cfe1>
2008-09-30 20:40:14 +0000

    fixing copyright

--- trunk/test/TDXmlTokenizer.m
+++ trunk/test/TDXmlTokenizer.m
@@ -3,7 +3,7 @@
 //  TDParseKit
 //
 //  Created by Todd Ditchendorf on 8/20/08.
-//  Copyright 2008 __MyCompanyName__. All rights reserved.
+//  Copyright 2008 Todd Ditchendorf. All rights reserved.
 //
 
 #import "TDXmlTokenizer.h"
