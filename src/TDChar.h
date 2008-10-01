5f7a653c392bc07830eab2b8cb3731fd1caee729
todd.ditchendorf <todd.ditchendorf@7c5183cd-b054-0410-b1f0-833f5c61cfe1>
2008-10-01 01:59:40 +0000

    more headerdocs - char parser stuff

--- src/TDChar.h
+++ src/TDChar.h
@@ -8,8 +8,20 @@
 
 #import <TDParseKit/TDTerminal.h>
 
+/*!
+    @class       TDChar 
+    @superclass  TDTerminal
+    @abstract    A <tt>TDChar</tt> matches a character from a character assembly.
+    @discussion  A <tt>TDChar</tt> matches a character from a character assembly. <tt>-[TDChar qualifies:]</tt> returns true every time, since this class assumes it is working against a <tt>TDCharacterAssembly</tt>.
+*/
 @interface TDChar : TDTerminal {
 
 }
+
+/*!
+	@method     char
+	@abstract   Convenience factory method for initializing an autoreleased <tt>TDChar</tt> parser.
+	@result     an initialized autoreleased <tt>TDChar</tt> parser.
+*/
 + (id)char;
 @end
--- src/TDDigit.h
+++ src/TDDigit.h
@@ -8,8 +8,20 @@
 
 #import <TDParseKit/TDTerminal.h>
 
+/*!
+    @class       TDDigit 
+    @superclass  TDTerminal
+    @abstract    A <tt>TDDigit</tt> matches a digit from a character assembly.
+    @discussion  A <tt>TDDigit</tt> matches a digit from a character assembly. <tt>-[TDDitgit qualifies:] returns true if an assembly's next element is a digit.
+*/
 @interface TDDigit : TDTerminal {
 
 }
+
+/*!
+	@method     digit
+	@abstract   Convenience factory method for initializing an autoreleased <tt>TDDigit</tt> parser.
+	@result     an initialized autoreleased <tt>TDDigit</tt> parser.
+*/
 + (id)digit;
 @end
