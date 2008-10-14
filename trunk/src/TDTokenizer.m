d4c0348ac06a5e96d9b8227796dadcc929158560
todd.ditchendorf <todd.ditchendorf@7c5183cd-b054-0410-b1f0-833f5c61cfe1>
2008-10-14 00:04:55 +0000

    consistency for uincode chars

--- trunk/src/TDTokenizer.m
+++ trunk/src/TDTokenizer.m
@@ -60,7 +60,7 @@
 		[self setTokenizerState:whitespaceState	from:   0 to: ' ']; // From:  0 to: 32	From:0x00 to:0x20
 		[self setTokenizerState:wordState		from: 'a' to: 'z']; // From: 97 to:122	From:0x61 to:0x7a
 		[self setTokenizerState:wordState		from: 'A' to: 'Z']; // From: 65 to: 90	From:0x41 to:0x5a
-		[self setTokenizerState:wordState		from:0xc0 to:0xff]; // From:192 to:255	From:0xc0 to:0xff
+		[self setTokenizerState:wordState		from:0xC0 to:0xFF]; // From:192 to:255	From:0xc0 to:0xff
 		[self setTokenizerState:numberState		from: '0' to: '9']; // From: 48 to: 57	From:0x30 to:0x39
 		[self setTokenizerState:numberState		from: '-' to: '-']; // From: 45 to: 45	From:0x2d to:0x2d
 		[self setTokenizerState:symbolState		from: '+' to: '+']; // 
