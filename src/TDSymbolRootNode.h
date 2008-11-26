7e7bb06dc0cd6ceea8607186acdfa4299ef36dc2
todd.ditchendorf <todd.ditchendorf@7c5183cd-b054-0410-b1f0-833f5c61cfe1>
2008-11-26 22:15:03 +0000

    doc style

--- src/TDSymbolRootNode.h
+++ src/TDSymbolRootNode.h
@@ -27,10 +27,10 @@
 - (void)add:(NSString *)s;
 
 /*!
- @fn         remove:
- @brief      Removes the given string as a multi-character symbol.
- @param      s a multi-character symbol that should no longer be recognized as a single symbol token by this state
- @details    if <tt>s</tt> was never added as a multi-character symbol, this has no effect
+    @fn         remove:
+    @brief      Removes the given string as a multi-character symbol.
+    @param      s a multi-character symbol that should no longer be recognized as a single symbol token by this state
+    @details    if <tt>s</tt> was never added as a multi-character symbol, this has no effect
 */
 - (void)remove:(NSString *)s;
 
