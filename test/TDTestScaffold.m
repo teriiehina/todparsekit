5576b433d1c8e5f1e98634f704422dcd6e39b3b6
todd.ditchendorf <todd.ditchendorf@7c5183cd-b054-0410-b1f0-833f5c61cfe1>
2008-12-10 18:50:59 +0000

    work on plist parser

--- test/TDTestScaffold.m
+++ test/TDTestScaffold.m
@@ -8,8 +8,8 @@
 
 #import "TDTestScaffold.h"
 
-#define RUN_ALL_TEST_CASES 1
-#define SOLO_TEST_CASE @"TDTokenTest"
+#define RUN_ALL_TEST_CASES 0
+#define SOLO_TEST_CASE @"TDPlistParserTest"
 
 @interface SenTestSuite (TDAdditions)
 - (void)addSuitesForClassNames:(NSArray *)classNames;
