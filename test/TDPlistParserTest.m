75b59d6b8ce9c6649b0f5e4f7c067be92b15c844
todd.ditchendorf <todd.ditchendorf@7c5183cd-b054-0410-b1f0-833f5c61cfe1>
2008-12-11 03:53:14 +0000

    copyright

--- test/TDPlistParserTest.m
+++ test/TDPlistParserTest.m
@@ -3,7 +3,7 @@
 //  TDParseKit
 //
 //  Created by Todd Ditchendorf on 12/9/08.
-//  Copyright 2008 __MyCompanyName__. All rights reserved.
+//  Copyright 2008 Todd Ditchendorf. All rights reserved.
 //
 
 #import "TDPlistParserTest.h"
