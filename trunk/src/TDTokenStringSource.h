5d549f1aee8570b7aebe662f51e822b34372b322
todd.ditchendorf <todd.ditchendorf@7c5183cd-b054-0410-b1f0-833f5c61cfe1>
2008-12-12 08:48:09 +0000

    adding header docs to TDTokenStringSource

--- trunk/src/TDTokenStringSource.h
+++ trunk/src/TDTokenStringSource.h
@@ -11,13 +11,51 @@
 @class TDTokenizer;
 @class TDToken;
 
+/*!
+    @class      TDTokenStringSource
+    @brief      A <tt>TokenStringSource</tt> enumerates over a specified reader, returning <tt>NSArray</tt>s of <tt>TDToken</tt>s delimited by a specified delimiter.
+    @details    For example,
+ 
+@code
+    NSString *s = @"I came; I saw; I left in peace;";
+
+    TDTokenizer *t = [TDTokenizer tokenizerWithString:s];
+    TDTokenStringSource tss = [[[TDTokenStringSource alloc] initWithTokenizer:t delimiter:@";"] autorelease];
+ 
+    while ([tss hasMore]) {
+        NSLog(@"%@", [tss nextTokenString]);
+    }
+@endcode
+ 
+ prints out:
+
+@endcode
+    I came
+    I saw
+    I left in peace
+@endcode
+*/
 @interface TDTokenStringSource : NSObject {
     TDTokenizer *tokenizer;
     NSString *delimiter;
     TDToken *nextToken;
 }
-
+/*!
+    @brief      Constructs a <tt>TDTokenStringSource</tt> that will read <tt>NSArray</tt>s of <tt>TDToken</tt>s using the specified tokenizer, delimited by the specified delimiter.
+    @param      tokenizer a tokenizer to read tokens from
+    @param      delimiter the character(s) that fences off where one array of tokens ends and the next begins
+*/
 - (id)initWithTokenizer:(TDTokenizer *)t delimiter:(NSString *)s;
+
+/*!
+    @brief      true if the source has more arrays of tokens.
+    @result     true, if the source has more arrays of tokens that have not yet been popped with <tt>-nextTokenString</tt>
+*/
 - (BOOL)hasMore;
+
+/*!
+    @brief      Returns the next array of tokens from the source.
+    @result     the next array of tokens from the source
+*/
 - (NSArray *)nextTokenString;
 @end
