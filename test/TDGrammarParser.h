32c10fa2665c245e98e608141fe6e2b9d77d601e
todd.ditchendorf <todd.ditchendorf@7c5183cd-b054-0410-b1f0-833f5c61cfe1>
2008-12-13 06:01:17 +0000

    adding GrammarParser adding new TDAssert macros cuz STAsserts r a PITA

---
+++ test/TDGrammarParser.h
@@ -0,0 +1,47 @@
+//
+//  TDGrammarParser.h
+//  TDParseKit
+//
+//  Created by Todd Ditchendorf on 12/12/08.
+//  Copyright 2008 Todd Ditchendorf All rights reserved.
+//
+
+#import <TDParseKit/TDParseKit.h>
+
+@interface TDGrammarParser : TDRepetition {
+    TDTokenizer *tokenizer;
+    TDCollectionParser *statementParser;
+    TDCollectionParser *exprOrAssignmentParser;
+    TDCollectionParser *assignmentParser;
+    TDCollectionParser *declarationParser;
+    TDCollectionParser *variableParser;
+    TDCollectionParser *expressionParser;
+    TDCollectionParser *termParser;
+    TDCollectionParser *orTermParser;
+    TDCollectionParser *factorParser;
+    TDCollectionParser *nextFactorParser;
+    TDCollectionParser *phraseParser;
+    TDCollectionParser *phraseStarParser;
+    TDCollectionParser *phraseQuestionParser;
+    TDCollectionParser *phrasePlusParser;
+    TDCollectionParser *atomicValueParser;
+}
+- (id)parse:(NSString *)s;
+
+@property (retain, readonly) TDTokenizer *tokenizer;
+@property (retain) TDCollectionParser *statementParser;
+@property (retain) TDCollectionParser *exprOrAssignmentParser;
+@property (retain) TDCollectionParser *assignmentParser;
+@property (retain) TDCollectionParser *declarationParser;
+@property (retain) TDCollectionParser *variableParser;
+@property (retain) TDCollectionParser *expressionParser;
+@property (retain) TDCollectionParser *termParser;
+@property (retain) TDCollectionParser *orTermParser;
+@property (retain) TDCollectionParser *factorParser;
+@property (retain) TDCollectionParser *nextFactorParser;
+@property (retain) TDCollectionParser *phraseParser;
+@property (retain) TDCollectionParser *phraseStarParser;
+@property (retain) TDCollectionParser *phraseQuestionParser;
+@property (retain) TDCollectionParser *phrasePlusParser;
+@property (retain) TDCollectionParser *atomicValueParser;
+@end
