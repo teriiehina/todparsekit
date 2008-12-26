638a40a08209d0f9fd412a32a939857fb2274508
todd.ditchendorf <todd.ditchendorf@7c5183cd-b054-0410-b1f0-833f5c61cfe1>
2008-12-26 02:04:38 +0000

    tests for simple css assembler

---
+++ trunk/test/TDSimpleCSSAssemblerTest.h
@@ -0,0 +1,18 @@
+//
+//  TDSimpleCSSAssemblerTest.h
+//  TDParseKit
+//
+//  Created by Todd Ditchendorf on 12/25/08.
+//  Copyright 2008 Todd Ditchendorf. All rights reserved.
+//
+
+#import "TDTestScaffold.h"
+#import "TDSimpleCSSAssembler.h"
+
+@interface TDSimpleCSSAssemblerTest : SenTestCase {
+    TDTokenAssembly *a;
+    NSString *s;
+    TDSimpleCSSAssembler *ass;
+}
+
+@end
