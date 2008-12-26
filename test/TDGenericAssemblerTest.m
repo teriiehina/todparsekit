03472c01b903b8cad5f70a6192d70f6b3aaddf83
todd.ditchendorf <todd.ditchendorf@7c5183cd-b054-0410-b1f0-833f5c61cfe1>
2008-12-26 09:42:01 +0000

    woops fixing some tests i broked

--- test/TDGenericAssemblerTest.m
+++ test/TDGenericAssemblerTest.m
@@ -43,20 +43,20 @@
     
     NSFont *font = [props objectForKey:NSFontAttributeName];
     TDNotNil(font);
-    TDEqualObjects([font familyName], @"Helvetica");
-    TDEquals((CGFloat)[font pointSize], (CGFloat)17.0);
+    TDEqualObjects([font familyName], @"Monaco");
+    TDEquals((CGFloat)[font pointSize], (CGFloat)11.0);
     
     NSColor *bgColor = [props objectForKey:NSBackgroundColorAttributeName];
     TDNotNil(bgColor);
-    STAssertEqualsWithAccuracy([bgColor redComponent], (CGFloat)0.0, 0.001, @"");
-    STAssertEqualsWithAccuracy([bgColor greenComponent], (CGFloat)0.0, 0.001, @"");
-    STAssertEqualsWithAccuracy([bgColor blueComponent], (CGFloat)1.0, 0.001, @"");
+    STAssertEqualsWithAccuracy([bgColor redComponent], (CGFloat)0.117, 0.001, @"");
+    STAssertEqualsWithAccuracy([bgColor greenComponent], (CGFloat)0.117, 0.001, @"");
+    STAssertEqualsWithAccuracy([bgColor blueComponent], (CGFloat)0.141, 0.001, @"");
     
     NSColor *color = [props objectForKey:NSForegroundColorAttributeName];
     TDNotNil(color);
-    STAssertEqualsWithAccuracy([color redComponent], (CGFloat)1.0, 0.001, @"");
-    STAssertEqualsWithAccuracy([color greenComponent], (CGFloat)0.0, 0.001, @"");
-    STAssertEqualsWithAccuracy([color blueComponent], (CGFloat)0.0, 0.001, @"");
+    STAssertEqualsWithAccuracy([color redComponent], (CGFloat)0.7, 0.001, @"");
+    STAssertEqualsWithAccuracy([color greenComponent], (CGFloat)0.14, 0.001, @"");
+    STAssertEqualsWithAccuracy([color blueComponent], (CGFloat)0.530, 0.001, @"");
     
 }
 
