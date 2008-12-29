5fb78926d55dbb671e25ff608abc13de313989ba
todd.ditchendorf <todd.ditchendorf@7c5183cd-b054-0410-b1f0-833f5c61cfe1>
2008-12-29 08:07:18 +0000

    some random  TDTokenizer

--- trunk/src/TDTokenizer.m
+++ trunk/src/TDTokenizer.m
@@ -10,10 +10,10 @@
 #import <TDParseKit/TDParseKit.h>
 
 @interface TDTokenizer ()
+- (void)addTokenizerState:(TDTokenizerState *)state from:(NSInteger)start to:(NSInteger)end;
+- (TDTokenizerState *)tokenizerStateFor:(NSInteger)c;
 @property (nonatomic, retain) TDReader *reader;
 @property (nonatomic, retain) NSMutableArray *tokenizerStates;
-
-- (TDTokenizerState *)tokenizerStateFor:(NSInteger)c;
 @end
 
 @implementation TDTokenizer
@@ -39,38 +39,41 @@
         self.string = s;
         self.reader = [[[TDReader alloc] init] autorelease];
         
-        self.numberState = [[[TDNumberState alloc] init] autorelease];
-        self.quoteState = [[[TDQuoteState alloc] init] autorelease];
-        self.commentState = [[[TDCommentState alloc] init] autorelease];
-        self.symbolState = [[[TDSymbolState alloc] init] autorelease];
-        self.whitespaceState = [[[TDWhitespaceState alloc] init] autorelease];
-        self.wordState = [[[TDWordState alloc] init] autorelease];
-
+        numberState = [[TDNumberState alloc] init];
+        quoteState = [[TDQuoteState alloc] init];
+        commentState = [[TDCommentState alloc] init];
+        symbolState = [[TDSymbolState alloc] init];
+        whitespaceState = [[TDWhitespaceState alloc] init];
+        wordState = [[TDWordState alloc] init];
+        
         [symbolState add:@"<="];
         [symbolState add:@">="];
         [symbolState add:@"!="];
         [symbolState add:@"=="];
-
+        
         [commentState addSingleLineStartSymbol:@"//"];
         [commentState addMultiLineStartSymbol:@"/*" endSymbol:@"*/"];
         
-        self.tokenizerStates = [NSMutableArray arrayWithCapacity:256];
-        NSInteger i = 0;
-        for ( ; i < 256; i++) {
-            [tokenizerStates addObject:symbolState];
-        }
+        tokenizerStates = [[NSMutableArray alloc] initWithCapacity:256];
         
-        [self setTokenizerState:whitespaceState from:   0 to: ' ']; // From:  0 to: 32    From:0x00 to:0x20
-        [self setTokenizerState:wordState       from: 'a' to: 'z']; // From: 97 to:122    From:0x61 to:0x7A
-        [self setTokenizerState:wordState       from: 'A' to: 'Z']; // From: 65 to: 90    From:0x41 to:0x5A
-        [self setTokenizerState:wordState       from:0xC0 to:0xFF]; // From:192 to:255    From:0xC0 to:0xFF
-        [self setTokenizerState:numberState     from: '0' to: '9']; // From: 48 to: 57    From:0x30 to:0x39
-        [self setTokenizerState:numberState     from: '-' to: '-']; // From: 45 to: 45    From:0x2D to:0x2D
-        [self setTokenizerState:symbolState     from: '+' to: '+']; // 
-        [self setTokenizerState:numberState     from: '.' to: '.']; // 
-        [self setTokenizerState:quoteState      from: '"' to: '"']; // From: 34 to: 34    From:0x22 to:0x22
-        [self setTokenizerState:quoteState      from:'\'' to:'\'']; // From: 39 to: 39    From:0x27 to:0x27
-        [self setTokenizerState:commentState    from: '/' to: '/']; // From: 47 to: 47    From:0x2F to:0x2F
+        [self addTokenizerState:whitespaceState from:   0 to: ' ']; // From:  0 to: 32    From:0x00 to:0x20
+        [self addTokenizerState:symbolState     from:  33 to:  33];
+        [self addTokenizerState:quoteState      from: '"' to: '"']; // From: 34 to: 34    From:0x22 to:0x22
+        [self addTokenizerState:symbolState     from:  35 to:  38];
+        [self addTokenizerState:quoteState      from:'\'' to:'\'']; // From: 39 to: 39    From:0x27 to:0x27
+        [self addTokenizerState:symbolState     from:  40 to:  42];
+        [self addTokenizerState:symbolState     from: '+' to: '+']; // From: 43 to: 43    From:0x2B to:0x2B
+        [self addTokenizerState:symbolState     from:  44 to:  44];
+        [self addTokenizerState:numberState     from: '-' to: '-']; // From: 45 to: 45    From:0x2D to:0x2D
+        [self addTokenizerState:numberState     from: '.' to: '.']; // From: 46 to: 46    From:0x2E to:0x2E
+        [self addTokenizerState:commentState    from: '/' to: '/']; // From: 47 to: 47    From:0x2F to:0x2F
+        [self addTokenizerState:numberState     from: '0' to: '9']; // From: 48 to: 57    From:0x30 to:0x39
+        [self addTokenizerState:symbolState     from:  58 to:  64];
+        [self addTokenizerState:wordState       from: 'A' to: 'Z']; // From: 65 to: 90    From:0x41 to:0x5A
+        [self addTokenizerState:symbolState     from:  91 to:  96];
+        [self addTokenizerState:wordState       from: 'a' to: 'z']; // From: 97 to:122    From:0x61 to:0x7A
+        [self addTokenizerState:symbolState     from: 123 to: 191];
+        [self addTokenizerState:wordState       from:0xC0 to:0xFF]; // From:192 to:255    From:0xC0 to:0xFF
     }
     return self;
 }
@@ -94,7 +97,7 @@
     NSInteger c = [reader read];
     
     TDToken *result = nil;
-
+    
     if (-1 == c) {
         result = [TDToken EOFToken];
     } else {
@@ -107,6 +110,15 @@
     }
     
     return result;
+}
+
+
+- (void)addTokenizerState:(TDTokenizerState *)state from:(NSInteger)start to:(NSInteger)end {
+    NSParameterAssert(state);
+    NSInteger i = start;
+    for ( ; i <= end; i++) {
+        [tokenizerStates addObject:state];
+    }
 }
 
 
