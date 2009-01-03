2440d4f0eeb18fcc6459cdfa0a2f3959586d8040
todd.ditchendorf <todd.ditchendorf@7c5183cd-b054-0410-b1f0-833f5c61cfe1>
2009-01-03 08:13:51 +0000

    adding JavaScript scripting interface to TDParseKit via JavaScriptCore

---
+++ test/TDJSToken.h
@@ -0,0 +1,16 @@
+//
+//  TDJSToken.h
+//  TDParseKit
+//
+//  Created by Todd Ditchendorf on 1/2/09.
+//  Copyright 2009 Todd Ditchendorf. All rights reserved.
+//
+
+#import <JavaScriptCore/JavaScriptCore.h>
+
+JSObjectRef TDToken_new(JSContextRef ctx, void *data);
+JSClassRef TDToken_class(JSContextRef ctx);
+JSObjectRef TDToken_construct(JSContextRef ctx, JSObjectRef constructor, size_t argc, const JSValueRef argv[], JSValueRef* ex);
+
+// a JS Class method
+JSValueRef TDToken_EOFToken(JSContextRef ctx, JSObjectRef function, JSObjectRef this, size_t argc, const JSValueRef argv[], JSValueRef* ex);
