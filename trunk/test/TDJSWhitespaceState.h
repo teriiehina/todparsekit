1de5a59c0d30883c7ebee3675b1e20494ee909b0
todd.ditchendorf <todd.ditchendorf@7c5183cd-b054-0410-b1f0-833f5c61cfe1>
2009-01-10 03:43:21 +0000

    more work on JSC scripting interface

---
+++ trunk/test/TDJSWhitespaceState.h
@@ -0,0 +1,13 @@
+//
+//  TDJSWhitespaceState.h
+//  TDParseKit
+//
+//  Created by Todd Ditchendorf on 1/9/09.
+//  Copyright 2009 Todd Ditchendorf. All rights reserved.
+//
+
+#import <JavaScriptCore/JavaScriptCore.h>
+
+JSObjectRef TDWhitespaceState_new(JSContextRef ctx, void *data);
+JSClassRef TDSWhitespaceState_class(JSContextRef ctx);
+JSObjectRef TDWhitespaceState_construct(JSContextRef ctx, JSObjectRef constructor, size_t argc, const JSValueRef argv[], JSValueRef* ex);
