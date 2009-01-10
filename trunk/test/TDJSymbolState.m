ea182590921bb677baacba9ab5058fc30150858b
todd.ditchendorf <todd.ditchendorf@7c5183cd-b054-0410-b1f0-833f5c61cfe1>
2009-01-10 03:33:26 +0000

    more work on JSC scripting interface

---
+++ trunk/test/TDJSymbolState.m
@@ -0,0 +1,103 @@
+//
+//  TDJSymbolState.m
+//  TDParseKit
+//
+//  Created by Todd Ditchendorf on 1/9/09.
+//  Copyright 2009 Todd Ditchendorf. All rights reserved.
+//
+
+#import "TDJSymbolState.h"
+#import "TDJSUtils.h"
+#import "TDJSTokenizerState.h"
+#import <TDParseKit/TDSymbolState.h>
+
+#pragma mark -
+#pragma mark Methods
+
+static JSValueRef TDSymbolState_toString(JSContextRef ctx, JSObjectRef function, JSObjectRef this, size_t argc, const JSValueRef argv[], JSValueRef *ex) {
+    return TDNSStringToJSValue(ctx, @"[object TDSymbolState]", ex);
+}
+
+//- (void)add:(NSString *)s;
+static JSValueRef TDSymbolState_add(JSContextRef ctx, JSObjectRef function, JSObjectRef this, size_t argc, const JSValueRef argv[], JSValueRef *ex) {
+    if (argc < 1) {
+        (*ex) = TDNSStringToJSValue(ctx, @"TDSymbolState.add() requires 1 arguments: symbolString", ex);
+        return JSValueMakeUndefined(ctx);
+    }
+    
+    NSString *s = TDJSValueGetNSString(ctx, argv[0], ex);
+    
+    TDSymbolState *data = JSObjectGetPrivate(this);
+    [data add:s];
+    
+    return JSValueMakeUndefined(ctx);
+}
+
+//- (void)remove:(NSString *)s;
+static JSValueRef TDSymbolState_remove(JSContextRef ctx, JSObjectRef function, JSObjectRef this, size_t argc, const JSValueRef argv[], JSValueRef *ex) {
+    if (argc < 1) {
+        (*ex) = TDNSStringToJSValue(ctx, @"TDSymbolState.remove() requires 1 arguments: symbolString", ex);
+        return JSValueMakeUndefined(ctx);
+    }
+    
+    NSString *s = TDJSValueGetNSString(ctx, argv[0], ex);
+    
+    TDSymbolState *data = JSObjectGetPrivate(this);
+    [data remove:s];
+    
+    return JSValueMakeUndefined(ctx);
+}
+
+#pragma mark -
+#pragma mark Properties
+
+#pragma mark -
+#pragma mark Initializer/Finalizer
+
+static void TDSymbolState_initialize(JSContextRef ctx, JSObjectRef this) {
+    
+}
+
+static void TDSymbolState_finalize(JSObjectRef this) {
+    TDTokenizer *data = (TDTokenizer *)JSObjectGetPrivate(this);
+    [data release];
+}
+
+//- (void)add:(NSString *)s;
+//- (void)remove:(NSString *)s;
+static JSStaticFunction TDSymbolState_staticFunctions[] = {
+{ "toString", TDSymbolState_toString, kJSPropertyAttributeDontDelete },
+{ "add", TDSymbolState_add, kJSPropertyAttributeDontDelete },
+{ "remove", TDSymbolState_remove, kJSPropertyAttributeDontDelete },
+{ 0, 0, 0 }
+};
+
+static JSStaticValue TDSymbolState_staticValues[] = {        
+{ 0, 0, 0, 0 }
+};
+
+#pragma mark -
+#pragma mark Public
+
+JSClassRef TDSymbolState_class(JSContextRef ctx) {
+    static JSClassRef jsClass = NULL;
+    if (!jsClass) {                
+        JSClassDefinition def = kJSClassDefinitionEmpty;
+        def.parentClass = TDTokenizerState_class(ctx);
+        def.staticFunctions = TDSymbolState_staticFunctions;
+        def.staticValues = TDSymbolState_staticValues;
+        def.initialize = TDSymbolState_initialize;
+        def.finalize = TDSymbolState_finalize;
+        jsClass = JSClassCreate(&def);
+    }
+    return jsClass;
+}
+
+JSObjectRef TDSymbolState_new(JSContextRef ctx, void *data) {
+    return JSObjectMake(ctx, TDSymbolState_class(ctx), data);
+}
+
+JSObjectRef TDSymbolState_construct(JSContextRef ctx, JSObjectRef constructor, size_t argc, const JSValueRef argv[], JSValueRef *ex) {
+    TDSymbolState *data = [[TDSymbolState alloc] init];
+    return TDSymbolState_new(ctx, data);
+}
