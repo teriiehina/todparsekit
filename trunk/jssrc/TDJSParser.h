7f406e4dae609f23bd1605ed9a2e8f2734339f61
todd.ditchendorf <todd.ditchendorf@7c5183cd-b054-0410-b1f0-833f5c61cfe1>
2009-01-11 06:39:10 +0000

    rearranging JSDemoApp

---
+++ trunk/jssrc/TDJSParser.h
@@ -0,0 +1,13 @@
+//
+//  TDJSParser.h
+//  TDParseKit
+//
+//  Created by Todd Ditchendorf on 1/10/09.
+//  Copyright 2009 Todd Ditchendorf. All rights reserved.
+//
+
+#import <JavaScriptCore/JavaScriptCore.h>
+
+JSObjectRef TDParser_new(JSContextRef ctx, void *data);
+JSClassRef TDParser_class(JSContextRef ctx);
+JSObjectRef TDParser_construct(JSContextRef ctx, JSObjectRef constructor, size_t argc, const JSValueRef argv[], JSValueRef* ex);
