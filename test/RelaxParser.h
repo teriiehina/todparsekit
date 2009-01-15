18753bdc49794412590a05a8bacd5575ff00424a
todd.ditchendorf <todd.ditchendorf@7c5183cd-b054-0410-b1f0-833f5c61cfe1>
2009-01-15 20:01:14 +0000

    adding RNC parser

---
+++ test/RelaxParser.h
@@ -0,0 +1,15 @@
+//
+//  RelaxParser.h
+//  TDParseKit
+//
+//  Created by Todd Ditchendorf on 1/15/09.
+//  Copyright 2009 Todd Ditchendorf. All rights reserved.
+//
+
+#import <TDParseKit/TDParseKit.h>
+
+@interface RelaxParser : TDSequence {
+
+}
+
+@end
