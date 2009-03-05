a11a5718f1e334ad12f21a3150ac19709e9ebdb8
todd.ditchendorf <todd.ditchendorf@7c5183cd-b054-0410-b1f0-833f5c61cfe1>
2009-03-05 00:52:56 +0000

    removing TDCharBufState stuff.
    it was dumb
    
    i was not using it

--- src/TDTokenizerState.h
+++ src/TDTokenizerState.h
@@ -8,8 +8,6 @@
 
 #import <Foundation/Foundation.h>
 
-#define TD_USE_MUTABLE_STRING_BUF 1
-
 @class TDToken;
 @class TDTokenizer;
 @class TDReader;
@@ -20,13 +18,7 @@
     @details    The tokenizer will typically have a character state table that decides which state to use, depending on an initial character. If a single character is insufficient, a state such as <tt>TDSlashState</tt> will read a second character, and may delegate to another state, such as <tt>TDSlashStarState</tt>. This prospect of delegation is the reason that the <tt>-nextToken</tt> method has a tokenizer argument.
 */
 @interface TDTokenizerState : NSObject {
-#if TD_USE_MUTABLE_STRING_BUF
     NSMutableString *stringbuf;
-#else
-    unichar *__strong charbuf;
-    NSUInteger length;
-    NSUInteger index;
-#endif
 }
 
 /*!
