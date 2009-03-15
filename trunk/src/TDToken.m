6debeaaacb3b11585bb6417d74c542314292ed26
todd.ditchendorf <todd.ditchendorf@7c5183cd-b054-0410-b1f0-833f5c61cfe1>
2009-03-15 23:44:29 +0000

    cleanup

--- trunk/src/TDToken.m
+++ trunk/src/TDToken.m
@@ -104,7 +104,7 @@ static TDTokenEOF *EOFToken = nil;
 // designated initializer
 - (id)initWithTokenType:(TDTokenType)t stringValue:(NSString *)s floatValue:(CGFloat)n {
     //NSParameterAssert(s);
-        if (self = [super init]) {
+    if (self = [super init]) {
         self.tokenType = t;
         self.stringValue = s;
         self.floatValue = n;
