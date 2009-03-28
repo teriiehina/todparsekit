b12dcaf767389f8760626eb911603272f2d11c17
todd.ditchendorf <todd.ditchendorf@7c5183cd-b054-0410-b1f0-833f5c61cfe1>
2009-03-28 03:15:51 +0000

    ok, js parser kinda working?

--- trunk/test/TDJavaScriptParserTest.m
+++ trunk/test/TDJavaScriptParserTest.m
@@ -323,6 +323,24 @@
 }
 
 
+//- (void)testArrayLiteral {
+//    s = @"var foo = ['bar'];";
+//    jsp.tokenizer.string = s;
+//    a = [TDTokenAssembly assemblyWithTokenizer:jsp.tokenizer];
+//    res = [jsp bestMatchFor:a];
+//    TDEqualObjects([res description], @"[var, foo, =, [, 'bar', ], ;]var/foo/=/[/'bar'/]/;^");    
+//}
+//
+//
+//- (void)testObjectLiteral {
+//    s = @"var foo = {foo:'bar'};";
+//    jsp.tokenizer.string = s;
+//    a = [TDTokenAssembly assemblyWithTokenizer:jsp.tokenizer];
+//    res = [jsp bestMatchFor:a];
+//    TDEqualObjects([res description], @"[var, foo, =, { foo, :, , 'bar', }, ;]var/foo/=/{/foo/:/'bar'/}/;^");    
+//}
+
+
 - (void)testSemi {
     s = @";";
     jsp.tokenizer.string = s;
