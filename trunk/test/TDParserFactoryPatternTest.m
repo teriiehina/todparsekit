272d71debbaa2f3da87d19750d9f965bf75502d0
todd.ditchendorf <todd.ditchendorf@7c5183cd-b054-0410-b1f0-833f5c61cfe1>
2009-06-06 21:46:44 +0000

    TDParserFactory: beginning to add grammar support for TDPattern

---
+++ trunk/test/TDParserFactoryPatternTest.m
@@ -0,0 +1,28 @@
+//
+//  TDParserFactoryPatternTest.m
+//  TDParseKit
+//
+//  Created by Todd Ditchendorf on 6/6/09.
+//  Copyright 2009 Todd Ditchendorf. All rights reserved.
+//
+
+#import "TDParserFactoryPatternTest.h"
+
+@implementation TDParserFactoryPatternTest
+
+- (void)setUp {
+    factory = [TDParserFactory factory];
+}
+
+
+- (void)test1 {
+    g = @"@start = ^foo^;";
+    lp = [factory parserFromGrammar:g assembler:nil];
+    TDNotNil(lp);
+    
+    s = @"foo";
+    res = [lp bestMatchFor:[TDTokenAssembly assemblyWithString:s]];
+    TDEqualObjects(@"[foo]foo^", [res description]);
+}
+
+@end
