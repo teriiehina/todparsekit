209b1286b3a581f242c008959bf140abb3cecb24
todd.ditchendorf <todd.ditchendorf@7c5183cd-b054-0410-b1f0-833f5c61cfe1>
2009-06-17 02:19:45 +0000

    fixing up DelimitedString support in Grammars

--- trunk/test/TDTestScaffold.m
+++ trunk/test/TDTestScaffold.m
@@ -8,8 +8,8 @@
 
 #import "TDTestScaffold.h"
 
-#define RUN_ALL_TEST_CASES 1
-#define SOLO_TEST_CASE @"TDParserFactoryPatternTest"
+#define RUN_ALL_TEST_CASES 0
+#define SOLO_TEST_CASE @"TDParserFactoryTest2"
 
 @interface SenTestSuite (TDAdditions)
 - (void)addSuitesForClassNames:(NSArray *)classNames;
