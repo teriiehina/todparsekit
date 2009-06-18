28e16b6dcc9920b0b26f619e7d3869a28dcf644f
todd.ditchendorf <todd.ditchendorf@7c5183cd-b054-0410-b1f0-833f5c61cfe1>
2009-06-18 04:31:58 +0000

    adding TDNSPredicateEvaluator

---
+++ trunk/test/TDNSPredicateEvaluator.h
@@ -0,0 +1,29 @@
+//
+//  TDNSPredicateEvaluator.h
+//  TDParseKit
+//
+//  Created by Todd Ditchendorf on 6/17/09.
+//  Copyright 2009 Todd Ditchendorf. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@class TDParser;
+@class TDNSPredicateEvaluator;
+
+@protocol TDKeyPathResolver <NSObject>
+- (id)resolveValueForKeyPath:(NSString *)s;
+- (CGFloat)resolveFloatForKeyPath:(NSString *)s;
+- (BOOL)resolveBoolForKeyPath:(NSString *)s;
+@end
+
+@interface TDNSPredicateEvaluator : NSObject {
+    id <TDKeyPathResolver>resolver;
+    TDParser *parser;
+}
+- (id)initWithKeyPathResolver:(id <TDKeyPathResolver>)r;
+
+- (BOOL)evaluate:(NSString *)s;
+
+@property (nonatomic, retain) TDParser *parser;
+@end
