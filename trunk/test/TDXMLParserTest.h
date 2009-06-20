e9fea37df6065ea8ce6949595511246ffde96180
todd.ditchendorf <todd.ditchendorf@7c5183cd-b054-0410-b1f0-833f5c61cfe1>
2009-06-20 08:40:33 +0000

    xml parser test stuff

---
+++ trunk/test/TDXMLParserTest.h
@@ -0,0 +1,20 @@
+//
+//  TDXMLParserTest.h
+//  TDParseKit
+//
+//  Created by Todd Ditchendorf on 6/19/09.
+//  Copyright 2009 Todd Ditchendorf. All rights reserved.
+//
+
+#import "TDTestScaffold.h"
+#import "TDParserFactory.h"
+
+@interface TDXMLParserTest : SenTestCase {
+    NSString *s;
+    TDTokenAssembly *a;
+    TDAssembly *res;
+    TDParser *p;
+    TDTokenizer *t;
+}
+
+@end
---
+++ trunk/test/TDXMLParserTest.m
@@ -0,0 +1,28 @@
+//
+//  TDXMLParserTest.m
+//  TDParseKit
+//
+//  Created by Todd Ditchendorf on 6/19/09.
+//  Copyright 2009 Todd Ditchendorf. All rights reserved.
+//
+
+#import "TDXMLParserTest.h"
+
+@implementation TDXMLParserTest
+
+- (void)setUp {
+	NSString *path = [[NSBundle bundleForClass:[self class]] pathForResource:@"xml" ofType:@"grammar"];
+	NSString *g = [NSString stringWithContentsOfFile:path];
+	p = [[TDParserFactory factory] parserFromGrammar:g assembler:self];
+    t = p.tokenizer;
+}
+
+
+- (void)test1 {
+	t.string = @"<foo/>";
+    res = [p bestMatchFor:[TDTokenAssembly assemblyWithTokenizer:t]];
+    TDEqualObjects(@"[<, foo, />]</foo//>^", [res description]);
+
+}
+
+@end
