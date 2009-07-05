abb2954bfb311ac15c5b97ad3eef2be9919c58ad
todd.ditchendorf <todd.ditchendorf@7c5183cd-b054-0410-b1f0-833f5c61cfe1>
2009-07-05 02:48:15 +0000

    xpath grammar tests

--- test/XPathParserGrammarTest.m
+++ test/XPathParserGrammarTest.m
@@ -38,208 +38,192 @@
 //    NSLog(@"\n\n res: %@ \n\n", res);
     TDEqualObjects(@"[/, foo]//foo^", [res description]);
     
-//    t.string = @"/foo/bar";
-//    res = [p completeMatchFor:[PKTokenAssembly assemblyWithTokenizer:t]];
-//    TDEqualObjects(@"[/, foo, /, bar]//foo///bar^", [res description]);
-//    
-//    t.string = @"/foo/bar/baz";
-//    res = [p completeMatchFor:[PKTokenAssembly assemblyWithTokenizer:t]];
-//    TDEqualObjects(@"[/, foo, /, bar, /, baz]//foo///bar///baz^", [res description]);
-//    
-//    t.string = @"/foo/bar[baz]";
-//    res = [p completeMatchFor:[PKTokenAssembly assemblyWithTokenizer:t]];
-//    TDEqualObjects(@"[/, foo, /, bar, [, baz, ]]//foo///bar/[/baz/]^", [res description]);
-//    
-//    t.string = @"/foo/bar[@baz]";
-//    res = [p completeMatchFor:[PKTokenAssembly assemblyWithTokenizer:t]];
-//    TDEqualObjects(@"[/, foo, /, bar, [, @, baz, ]]//foo///bar/[/@/baz/]^", [res description]);
-//    
-//    t.string = @"/foo/bar[@baz='foo']";
-//    res = [p completeMatchFor:[PKTokenAssembly assemblyWithTokenizer:t]];
-//    TDEqualObjects(@"[/, foo, /, bar, [, @, baz, =, 'foo', ]]//foo///bar/[/@/baz/=/'foo'/]^", [res description]);
-//    
-//    t.string = @"/foo/bar[baz]/foo";
-//    res = [p completeMatchFor:[PKTokenAssembly assemblyWithTokenizer:t]];
-//    TDEqualObjects(@"[/, foo, /, bar, [, baz, ], /, foo]//foo///bar/[/baz/]///foo^", [res description]);
-//    
-//    // not supported
-//    //    t.string = @"//foo";
-//    //    a = [PKTokenAssembly assemblyWithTokenizer:t];
-//    //    res = [p bestMatchFor:a];
-//    //    NSLog(@"\n\n res: %@ \n\n", res);
-//    //    TDEqualObjects(@"[//, foo]///foo^", [res description]);
+    t.string = @"/foo/bar";
+    res = [p completeMatchFor:[PKTokenAssembly assemblyWithTokenizer:t]];
+    TDEqualObjects(@"[/, foo, /, bar]//foo///bar^", [res description]);
+    
+    t.string = @"/foo/bar/baz";
+    res = [p completeMatchFor:[PKTokenAssembly assemblyWithTokenizer:t]];
+    TDEqualObjects(@"[/, foo, /, bar, /, baz]//foo///bar///baz^", [res description]);
+    
+    t.string = @"/foo/bar[baz]";
+    res = [p completeMatchFor:[PKTokenAssembly assemblyWithTokenizer:t]];
+    TDEqualObjects(@"[/, foo, /, bar, [, baz, ]]//foo///bar/[/baz/]^", [res description]);
+    
+    t.string = @"/foo/bar[@baz]";
+    res = [p completeMatchFor:[PKTokenAssembly assemblyWithTokenizer:t]];
+    TDEqualObjects(@"[/, foo, /, bar, [, @, baz, ]]//foo///bar/[/@/baz/]^", [res description]);
+    
+    t.string = @"/foo/bar[@baz='foo']";
+    res = [p completeMatchFor:[PKTokenAssembly assemblyWithTokenizer:t]];
+    TDEqualObjects(@"[/, foo, /, bar, [, @, baz, =, 'foo', ]]//foo///bar/[/@/baz/=/'foo'/]^", [res description]);
+    
+    t.string = @"/foo/bar[baz]/foo";
+    res = [p completeMatchFor:[PKTokenAssembly assemblyWithTokenizer:t]];
+    TDEqualObjects(@"[/, foo, /, bar, [, baz, ], /, foo]//foo///bar/[/baz/]///foo^", [res description]);
+    
+    // not supported
+    //    t.string = @"//foo";
+    //    a = [PKTokenAssembly assemblyWithTokenizer:t];
+    //    res = [p bestMatchFor:a];
+    //    NSLog(@"\n\n res: %@ \n\n", res);
+    //    TDEqualObjects(@"[//, foo]///foo^", [res description]);
 }
 
 
-//- (void)testAxisName {
-//    t.string = @"child";
-//    a = [PKTokenAssembly assemblyWithTokenizer:t];
-//    NSLog(@"\n\n a: %@ \n\n", a);
-//    res = [[p parserNamed:@"axisName"] bestMatchFor:a];
-//    TDNotNil(res);
-//    TDEqualObjects(@"[child]child^", [res description]);
+- (void)testAxisName {
+    t.string = @"child";
+    a = [PKTokenAssembly assemblyWithTokenizer:t];
+    res = [[p parserNamed:@"axisName"] bestMatchFor:a];
+    TDNotNil(res);
+    TDEqualObjects(@"[child]child^", [res description]);
     
-//    t.string = @"preceeding-sibling";
-//    a = [PKTokenAssembly assemblyWithTokenizer:t];
-//    res = [[p parserNamed:@"axisName"] bestMatchFor:a];
-//    TDNotNil(res);
-//    TDEqualObjects(@"[preceeding-sibling]preceeding-sibling^", [res description]);
-//}
+    t.string = @"preceding-sibling";
+    a = [PKTokenAssembly assemblyWithTokenizer:t];
+    res = [[p parserNamed:@"axisName"] bestMatchFor:a];
+    TDNotNil(res);
+    TDEqualObjects(@"[preceding-sibling]preceding-sibling^", [res description]);
+}
 
 
-//- (void)testAxisSpecifier {
-//    t.string = @"child::";
-//    a = [PKTokenAssembly assemblyWithTokenizer:t];
-//    res = [[p parserNamed:@"axisSpecifier"] bestMatchFor:a];
-//    TDNotNil(res);
-//    TDEqualObjects(@"[child, ::]child/::^", [res description]);
-//    t.string = @"preceeding-sibling::";
-//    a = [PKTokenAssembly assemblyWithTokenizer:t];
-//    res = [[p parserNamed:@"axisSpecifier"] bestMatchFor:a];
-//    TDNotNil(res);
-//    TDEqualObjects(@"[preceeding-sibling, ::]preceeding-sibling/::^", [res description]);
-//}
+- (void)testAxisSpecifier {
+    t.string = @"child::";
+    a = [PKTokenAssembly assemblyWithTokenizer:t];
+    res = [[p parserNamed:@"axisSpecifier"] bestMatchFor:a];
+    TDNotNil(res);
+    TDEqualObjects(@"[child, ::]child/::^", [res description]);
+    t.string = @"preceding-sibling::";
+    a = [PKTokenAssembly assemblyWithTokenizer:t];
+    res = [[p parserNamed:@"axisSpecifier"] bestMatchFor:a];
+    TDNotNil(res);
+    TDEqualObjects(@"[preceding-sibling, ::]preceding-sibling/::^", [res description]);
+}
 
 
-//- (void)testOperatorName {
-//    t.string = @"and";
-//    a = [PKTokenAssembly assemblyWithTokenizer:t];
-//    res = [[p parserNamed:@"operatorName"] bestMatchFor:a];
-//    TDNotNil(res);
-//    TDEqualObjects(@"[and]and^", [res description]);
-//    
-//    t.string = @"or";
-//    a = [PKTokenAssembly assemblyWithTokenizer:t];
-//    res = [[p parserNamed:@"operatorName"] bestMatchFor:a];
-//    TDNotNil(res);
-//    TDEqualObjects(@"[or]or^", [res description]);
-//}
-//
-//
-//- (void)testQName {
-//    t.string = @"foo:bar";
-//    a = [PKTokenAssembly assemblyWithTokenizer:t];
-//    res = [[p parserNamed:@"QName"] bestMatchFor:a];
-//    TDNotNil(res);
-//    TDEqualObjects(@"[foo, :, bar]foo/:/bar^", [res description]);
-//    
-//    t.string = @"foo:bar";
-//    a = [PKTokenAssembly assemblyWithTokenizer:t];
-//    //TDAssertThrowsSpecificNamed([p.QName bestMatchFor:a], [NSException class], @"PKTrackException");
-//}
+- (void)testQName {
+    t.string = @"foo:bar";
+    a = [PKTokenAssembly assemblyWithTokenizer:t];
+    res = [[p parserNamed:@"qName"] bestMatchFor:a];
+    TDNotNil(res);
+    TDEqualObjects(@"[foo, :, bar]foo/:/bar^", [res description]);
+    
+    t.string = @"foo:bar";
+    a = [PKTokenAssembly assemblyWithTokenizer:t];
+    //TDAssertThrowsSpecificNamed([p.QName bestMatchFor:a], [NSException class], @"PKTrackException");
+}
 
-//
-//- (void)testNameTest {
-//    t.string = @"foo:bar";
-//    a = [PKTokenAssembly assemblyWithTokenizer:t];
-//    res = [[p parserNamed:@"nameTest"] bestMatchFor:a];
-//    TDNotNil(res);
-//    TDEqualObjects(@"[foo, :, bar]foo/:/bar^", [res description]);
-//    
-//    t.string = @"*";
-//    a = [PKTokenAssembly assemblyWithTokenizer:t];
-//    res = [[p parserNamed:@"nameTest"] bestMatchFor:a];
-//    TDNotNil(res);
-//    TDEqualObjects(@"[*]*^", [res description]);
-//    
-//    t.string = @"foo:*";
-//    a = [PKTokenAssembly assemblyWithTokenizer:t];
-//    res = [[p parserNamed:@"nameTest"] bestMatchFor:a];
-//    TDNotNil(res);
-//    TDEqualObjects(@"[foo, :, *]foo/:/*^", [res description]);
-//    
-//    t.string = @"*:bar"; // NOT ALLOWED
-//    a = [PKTokenAssembly assemblyWithTokenizer:t];
-//    res = [[p parserNamed:@"nameTest"] bestMatchFor:a];
-//    TDNotNil(res);
-//    TDEqualObjects(@"[*]*^:/bar", [res description]);
-//    
-//    t.string = @"foo";
-//    a = [PKTokenAssembly assemblyWithTokenizer:t];
-//    res = [[p parserNamed:@"nameTest"] bestMatchFor:a];
-//    TDNotNil(res);
-//    TDEqualObjects(@"[foo]foo^", [res description]);
-//    
-//    t.string = @"foo:bar";
-//    a = [PKTokenAssembly assemblyWithTokenizer:t];
-//    //TDAssertThrowsSpecificNamed([p.nameTest bestMatchFor:a], [NSException class], @"PKTrackException");
-//}
-//
-//
-//- (void)testNodeType {
-//    t.string = @"comment";
-//    a = [PKTokenAssembly assemblyWithTokenizer:t];
-//    res = [[p parserNamed:@"nodeType"] bestMatchFor:a];
-//    TDNotNil(res);
-//    TDEqualObjects(@"[comment]comment^", [res description]);
-//    
-//    t.string = @"node";
-//    a = [PKTokenAssembly assemblyWithTokenizer:t];
-//    res = [[p parserNamed:@"nodeType"] bestMatchFor:a];
-//    TDNotNil(res);
-//    TDEqualObjects(@"[node]node^", [res description]);
-//    
-//}
-//
-//
-//- (void)testNodeTest {
-//    t.string = @"comment()";
-//    a = [PKTokenAssembly assemblyWithTokenizer:t];
-//    res = [[p parserNamed:@"nodeTest"] bestMatchFor:a];
-//    TDNotNil(res);
-//    TDEqualObjects(@"[comment, (, )]comment/(/)^", [res description]);
-//    
-//    t.string = @"processing-instruction()";
-//    a = [PKTokenAssembly assemblyWithTokenizer:t];
-//    res = [[p parserNamed:@"nodeTest"] bestMatchFor:a];
-//    TDNotNil(res);
-//    TDEqualObjects(@"[processing-instruction, (, )]processing-instruction/(/)^", [res description]);
-//    
-//    t.string = @"processing-instruction('baz')";
-//    a = [PKTokenAssembly assemblyWithTokenizer:t];
-//    res = [[p parserNamed:@"nodeTest"] bestMatchFor:a];
-//    TDNotNil(res);
-//    TDEqualObjects(@"[processing-instruction, (, 'baz', )]processing-instruction/(/'baz'/)^", [res description]);
-//    
-//    t.string = @"node()";
-//    a = [PKTokenAssembly assemblyWithTokenizer:t];
-//    res = [[p parserNamed:@"nodeTest"] bestMatchFor:a];
-//    TDNotNil(res);
-//    TDEqualObjects(@"[node, (, )]node/(/)^", [res description]);
-//    
-//    t.string = @"text()";
-//    a = [PKTokenAssembly assemblyWithTokenizer:t];
-//    res = [[p parserNamed:@"nodeTest"] bestMatchFor:a];
-//    TDNotNil(res);
-//    TDEqualObjects(@"[text, (, )]text/(/)^", [res description]);
-//    
-//    t.string = @"*";
-//    a = [PKTokenAssembly assemblyWithTokenizer:t];
-//    res = [[p parserNamed:@"nodeTest"] bestMatchFor:a];
-//    TDNotNil(res);
-//    TDEqualObjects(@"[*]*^", [res description]);
-//    
-//    t.string = @"foo:*";
-//    a = [PKTokenAssembly assemblyWithTokenizer:t];
-//    res = [[p parserNamed:@"nodeTest"] bestMatchFor:a];
-//    TDNotNil(res);
-//    TDEqualObjects(@"[foo, :, *]foo/:/*^", [res description]);
-//    
-//    t.string = @"bar";
-//    a = [PKTokenAssembly assemblyWithTokenizer:t];
-//    res = [[p parserNamed:@"nodeTest"] bestMatchFor:a];
-//    TDNotNil(res);
-//    TDEqualObjects(@"[bar]bar^", [res description]);
-//}
-//
-//
-//- (void)testVariableReference {
+
+- (void)testNameTest {
+    t.string = @"foo:bar";
+    a = [PKTokenAssembly assemblyWithTokenizer:t];
+    res = [[p parserNamed:@"nameTest"] bestMatchFor:a];
+    TDNotNil(res);
+    TDEqualObjects(@"[foo, :, bar]foo/:/bar^", [res description]);
+    
+    t.string = @"*";
+    a = [PKTokenAssembly assemblyWithTokenizer:t];
+    res = [[p parserNamed:@"nameTest"] bestMatchFor:a];
+    TDNotNil(res);
+    TDEqualObjects(@"[*]*^", [res description]);
+    
+    t.string = @"foo:*";
+    a = [PKTokenAssembly assemblyWithTokenizer:t];
+    res = [[p parserNamed:@"nameTest"] bestMatchFor:a];
+    TDNotNil(res);
+    TDEqualObjects(@"[foo, :, *]foo/:/*^", [res description]);
+    
+    t.string = @"*:bar"; // NOT ALLOWED
+    a = [PKTokenAssembly assemblyWithTokenizer:t];
+    res = [[p parserNamed:@"nameTest"] bestMatchFor:a];
+    TDNotNil(res);
+    TDEqualObjects(@"[*]*^:/bar", [res description]);
+    
+    t.string = @"foo";
+    a = [PKTokenAssembly assemblyWithTokenizer:t];
+    res = [[p parserNamed:@"nameTest"] bestMatchFor:a];
+    TDNotNil(res);
+    TDEqualObjects(@"[foo]foo^", [res description]);
+    
+    t.string = @"foo:bar";
+    a = [PKTokenAssembly assemblyWithTokenizer:t];
+    //TDAssertThrowsSpecificNamed([p.nameTest bestMatchFor:a], [NSException class], @"PKTrackException");
+}
+
+
+- (void)testNodeType {
+    t.string = @"comment";
+    a = [PKTokenAssembly assemblyWithTokenizer:t];
+    res = [[p parserNamed:@"nodeType"] bestMatchFor:a];
+    TDNotNil(res);
+    TDEqualObjects(@"[comment]comment^", [res description]);
+    
+    t.string = @"node";
+    a = [PKTokenAssembly assemblyWithTokenizer:t];
+    res = [[p parserNamed:@"nodeType"] bestMatchFor:a];
+    TDNotNil(res);
+    TDEqualObjects(@"[node]node^", [res description]);
+    
+}
+
+
+- (void)testNodeTest {
+    t.string = @"comment()";
+    a = [PKTokenAssembly assemblyWithTokenizer:t];
+    res = [[p parserNamed:@"nodeTest"] bestMatchFor:a];
+    TDNotNil(res);
+    TDEqualObjects(@"[comment, (, )]comment/(/)^", [res description]);
+    
+    t.string = @"processing-instruction()";
+    a = [PKTokenAssembly assemblyWithTokenizer:t];
+    res = [[p parserNamed:@"nodeTest"] bestMatchFor:a];
+    TDNotNil(res);
+    TDEqualObjects(@"[processing-instruction, (, )]processing-instruction/(/)^", [res description]);
+    
+    t.string = @"processing-instruction('baz')";
+    a = [PKTokenAssembly assemblyWithTokenizer:t];
+    res = [[p parserNamed:@"nodeTest"] bestMatchFor:a];
+    TDNotNil(res);
+    TDEqualObjects(@"[processing-instruction, (, 'baz', )]processing-instruction/(/'baz'/)^", [res description]);
+    
+    t.string = @"node()";
+    a = [PKTokenAssembly assemblyWithTokenizer:t];
+    res = [[p parserNamed:@"nodeTest"] bestMatchFor:a];
+    TDNotNil(res);
+    TDEqualObjects(@"[node, (, )]node/(/)^", [res description]);
+    
+    t.string = @"text()";
+    a = [PKTokenAssembly assemblyWithTokenizer:t];
+    res = [[p parserNamed:@"nodeTest"] bestMatchFor:a];
+    TDNotNil(res);
+    TDEqualObjects(@"[text, (, )]text/(/)^", [res description]);
+    
+    t.string = @"*";
+    a = [PKTokenAssembly assemblyWithTokenizer:t];
+    res = [[p parserNamed:@"nodeTest"] bestMatchFor:a];
+    TDNotNil(res);
+    TDEqualObjects(@"[*]*^", [res description]);
+    
+    t.string = @"foo:*";
+    a = [PKTokenAssembly assemblyWithTokenizer:t];
+    res = [[p parserNamed:@"nodeTest"] bestMatchFor:a];
+    TDNotNil(res);
+    TDEqualObjects(@"[foo, :, *]foo/:/*^", [res description]);
+    
+    t.string = @"bar";
+    a = [PKTokenAssembly assemblyWithTokenizer:t];
+    res = [[p parserNamed:@"nodeTest"] bestMatchFor:a];
+    TDNotNil(res);
+    TDEqualObjects(@"[bar]bar^", [res description]);
+}
+
+
+- (void)testVariableReference {
 //    t.string = @"$foo";
 //    a = [PKTokenAssembly assemblyWithTokenizer:t];
 //    res = [[p parserNamed:@"variableReference"] bestMatchFor:a];
 //    TDNotNil(res);
 //    TDEqualObjects(@"[$, foo]$/foo^", [res description]);
-//    
+    
 //    t.string = @"$bar";
 //    a = [PKTokenAssembly assemblyWithTokenizer:t];
 //    res = [[p parserNamed:@"variableReference"] bestMatchFor:a];
@@ -251,16 +235,16 @@
 //    res = [[p parserNamed:@"variableReference"] bestMatchFor:a];
 //    TDNotNil(res);
 //    TDEqualObjects(@"[$, foo, :, bar]$/foo/:/bar^", [res description]);
-//}
-//
-//
-//- (void)testFunctionCall {
+}
+
+
+- (void)testFunctionCall {
 //    t.string = @"foo()";
 //    a = [PKTokenAssembly assemblyWithTokenizer:t];
 //    res = [[p parserNamed:@"functionCall"] bestMatchFor:a];
 //    TDNotNil(res);
 //    TDEqualObjects(@"[foo, (, )]foo/(/)^", [res description]);
-//    
+    
 //    t.string = @"foo('bar')";
 //    a = [PKTokenAssembly assemblyWithTokenizer:t];
 //    res = [[p parserNamed:@"functionCall"] bestMatchFor:a];
@@ -278,29 +262,29 @@
 //    res = [[p parserNamed:@"functionCall"] bestMatchFor:a];
 //    TDNotNil(res);
 //    TDEqualObjects(@"[foo, (, 'bar', ,, 1, )]foo/(/'bar'/,/1/)^", [res description]);
-//}
-//
-//- (void)testOrExpr {
-//    t.string = @"foo or bar";
-//    a = [PKTokenAssembly assemblyWithTokenizer:t];
-//    res = [[p parserNamed:@"orExpr"] bestMatchFor:a];
-//    TDNotNil(res);
-//    TDEqualObjects(@"[foo, or, bar]foo/or/bar^", [res description]);
-//}
-//
-//
-//- (void)testAndExpr {
-//    t.string = @"foo() and bar()";
-//    a = [PKTokenAssembly assemblyWithTokenizer:t];
-//    res = [[p parserNamed:@"andExpr"] bestMatchFor:a];
-//    TDNotNil(res);
-//    TDEqualObjects(@"[foo, (, ), and, bar, (, )]foo/(/)/and/bar/(/)^", [res description]);
-//    
-//    t.string = @"foo and bar";
-//    a = [PKTokenAssembly assemblyWithTokenizer:t];
-//    res = [[p parserNamed:@"andExpr"] bestMatchFor:a];
-//    TDNotNil(res);
-//    TDEqualObjects(@"[foo, and, bar]foo/and/bar^", [res description]);
-//}
+}
+
+- (void)testOrExpr {
+    t.string = @"foo or bar";
+    a = [PKTokenAssembly assemblyWithTokenizer:t];
+    res = [[p parserNamed:@"orExpr"] bestMatchFor:a];
+    TDNotNil(res);
+    TDEqualObjects(@"[foo, or, bar]foo/or/bar^", [res description]);
+}
+
+
+- (void)testAndExpr {
+    t.string = @"foo() and bar()";
+    a = [PKTokenAssembly assemblyWithTokenizer:t];
+    res = [[p parserNamed:@"andExpr"] bestMatchFor:a];
+    TDNotNil(res);
+    TDEqualObjects(@"[foo, (, ), and, bar, (, )]foo/(/)/and/bar/(/)^", [res description]);
+    
+    t.string = @"foo and bar";
+    a = [PKTokenAssembly assemblyWithTokenizer:t];
+    res = [[p parserNamed:@"andExpr"] bestMatchFor:a];
+    TDNotNil(res);
+    TDEqualObjects(@"[foo, and, bar]foo/and/bar^", [res description]);
+}
 
 @end
