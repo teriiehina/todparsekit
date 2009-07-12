4d9e8ef9c299073092d65b60ec7df8c0b2c76a17
todd.ditchendorf <todd.ditchendorf@7c5183cd-b054-0410-b1f0-833f5c61cfe1>
2009-07-12 23:33:55 +0000

    adding -[PKTokenizerState setFallbackState:from:to:]
     and tests

--- test/TDTestScaffold.m
+++ test/TDTestScaffold.m
@@ -9,7 +9,7 @@
 #import "TDTestScaffold.h"
 
 #define RUN_ALL_TEST_CASES 1
-#define SOLO_TEST_CASE @"TDArithmeticParserTest"
+#define SOLO_TEST_CASE @"TDTokenizerStateTest"
 
 @interface SenTestSuite (TDAdditions)
 - (void)addSuitesForClassNames:(NSArray *)classNames;
