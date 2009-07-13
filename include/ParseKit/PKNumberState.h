236642616e2adfb0493ab0bd6089a99dfa5a16fc
todd.ditchendorf <todd.ditchendorf@7c5183cd-b054-0410-b1f0-833f5c61cfe1>
2009-07-13 06:09:55 +0000

    docs

--- include/ParseKit/PKNumberState.h
+++ include/ParseKit/PKNumberState.h
@@ -13,7 +13,7 @@
     @class      PKNumberState 
     @brief      A number state returns a number from a reader.
     @details    This state's idea of a number allows an optional, initial minus sign, followed by one or more digits. A decimal point and another string of digits may follow these digits.
-                If <tt>allowsScientificNotation</tt> is true (default is false) this state allows 'e' or 'E' followed by an (optionally explicityly positive or negative) integer to represent 10 to the indicated power. For example, this state will recognize <tt>1e2</tt> as equaling <tt>100</tt>.</p>
+                If <tt>allowsScientificNotation</tt> is YES (default is NO) this state allows 'e' or 'E' followed by an (optionally explicityly positive or negative) integer to represent 10 to the indicated power. For example, this state will recognize <tt>1e2</tt> as equaling <tt>100</tt>.</p>
 */
 @interface PKNumberState : PKTokenizerState {
     BOOL allowsTrailingDot;
@@ -36,28 +36,28 @@
 
 /*!
     @property   allowsTrailingDot
-    @brief      If true, numbers are allowed to end with a trialing dot, e.g. <tt>42.<tt>
+    @brief      If YES, numbers are allowed to end with a trialing dot, e.g. <tt>42.<tt>
     @details    default is NO
 */
 @property (nonatomic) BOOL allowsTrailingDot;
 
 /*!
     @property   allowsScientificNotation
-    @brief      If true, supports exponential numbers like <tt>42.0e2<tt>, <tt>2E+6<tt>, or <tt>5.1e-6<tt>
+    @brief      If YES, supports exponential numbers like <tt>42.0e2<tt>, <tt>2E+6<tt>, or <tt>5.1e-6<tt>
     @details    default is NO
 */
 @property (nonatomic) BOOL allowsScientificNotation;
 
 /*!
     @property   allowsOctalNotation
-    @brief      If true, supports octal numbers like <tt>020<tt> (16), or <tt>0102<tt> (66)
+    @brief      If YES, supports octal numbers like <tt>020<tt> (16), or <tt>0102<tt> (66)
     @details    default is NO
 */
 @property (nonatomic) BOOL allowsOctalNotation;
 
 /*!
     @property   allowsHexidecimalNotation
-    @brief      If true, supports hex numbers like <tt>0x20<tt> (32), or <tt>0x42<tt> (66)
+    @brief      If YES, supports hex numbers like <tt>0x20<tt> (32), or <tt>0x42<tt> (66)
     @details    default is NO
 */
 @property (nonatomic) BOOL allowsHexidecimalNotation;
