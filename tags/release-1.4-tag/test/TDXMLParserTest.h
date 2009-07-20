e20b813bb45b2e3a9aa4395e022696b75468ade6
todd.ditchendorf <todd.ditchendorf@7c5183cd-b054-0410-b1f0-833f5c61cfe1>
2009-07-20 00:01:11 +0000

    Creating 1.4 release tag from -r1249

---
+++ tags/release-1.4-tag/test/TDXMLParserTest.h
@@ -0,0 +1,22 @@
+//
+//  PKXMLParserTest.h
+//  ParseKit
+//
+//  Created by Todd Ditchendorf on 6/19/09.
+//  Copyright 2009 Todd Ditchendorf. All rights reserved.
+//
+
+#import "TDTestScaffold.h"
+#import "PKParserFactory.h"
+
+@interface TDXMLParserTest : SenTestCase {
+    NSString *s;
+    NSString *g;
+    PKParserFactory *factory;
+    PKTokenAssembly *a;
+    PKAssembly *res;
+    PKParser *p;
+    PKTokenizer *t;
+}
+
+@end
