a968b387b51b46d5adf74fcd0ee9c7eec44f6590
todd.ditchendorf <todd.ditchendorf@7c5183cd-b054-0410-b1f0-833f5c61cfe1>
2009-08-03 00:37:31 +0000

    cleanup

--- trunk/src/PKParseTreeAssembler.m
+++ trunk/src/PKParseTreeAssembler.m
@@ -75,11 +75,11 @@
     return nil;
 }
 
-- (NSString *)ruleNameForSelName:(NSString *)selName withPrefix:(NSString *)pre {
+- (NSString *)ruleNameForSelName:(NSString *)selName withPrefix:(NSString *)prefix {
     NSString *ruleName = [ruleNames objectForKey:selName];
     
     if (!ruleName) {
-        NSUInteger prefixLen = pre.length;
+        NSUInteger prefixLen = prefix.length;
         NSInteger c = ((NSInteger)[selName characterAtIndex:prefixLen]) + 32; // lowercase
         NSRange r = NSMakeRange(prefixLen + 1, selName.length - (prefixLen + suffix.length + 1 /*:*/));
         ruleName = [NSString stringWithFormat:@"%C%@", c, [selName substringWithRange:r]];
