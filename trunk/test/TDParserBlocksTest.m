1d715508eac7a162d38ea0f7231ecdb2df138f3c
todd.ditchendorf <todd.ditchendorf@7c5183cd-b054-0410-b1f0-833f5c61cfe1>
2009-09-18 01:39:17 +0000

    tests

--- trunk/test/TDParserBlocksTest.m
+++ trunk/test/TDParserBlocksTest.m
@@ -22,7 +22,7 @@
 #if !TARGET_OS_IPHONE
 - (void)testMath {
     s = @"2 4 6 8";
-    start= [PKTokenAssembly assemblyWithString:s];
+    start = [PKTokenAssembly assemblyWithString:s];
     
     PKNumber *n = [PKNumber number];
     p = [PKRepetition repetitionWithSubparser:n];
@@ -52,6 +52,33 @@
     TDNotNil(result);
     TDEqualObjects(@"[20]2/4/6/8^", [result description]);
     TDEquals((double)20.0, [[result pop] doubleValue]);
+}
+
+
+- (void)testMath2 {
+    PKParser *addParser = [PKRepetition repetitionWithSubparser:[PKNumber number]];
+    
+    addParser.assemblerBlock = ^(PKAssembly *a) {
+        NSArray *toks = [a objectsAbove:nil];
+        double total = 0.0;
+        
+        for (PKToken *tok in toks) {
+            total += [tok floatValue];
+        }
+        
+        [a push:[NSNumber numberWithDouble:total]];
+    };
+    
+    s = @"2.5 -5.5 8";
+
+//    NSNumber *result = [addParser parse:s];
+//    NSAssert([result doubleValue] == 5.0, @"");
+
+    start = [PKTokenAssembly assemblyWithString:s];
+    PKAssembly *result = [addParser completeMatchFor:start];
+    TDNotNil(result);
+    TDEqualObjects(@"[5]2.5/-5.5/8^", [result description]);
+    TDEquals(5.0, [(NSNumber *)[result pop] doubleValue]);
 }
 #endif
 #endif
